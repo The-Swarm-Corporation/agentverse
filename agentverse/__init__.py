392a903b2e9e01734717bee88c11fcc4218cb7aa
Your Name <you@example.com>
2024-10-30 11:48:31 -0400

    [CLEANUP]

---
+++ agentverse/__init__.py
@@ -0,0 +1,4 @@
+from agentverse.main import OpenInterpreterAgent
+from agentverse.multion_agent import MultiOnAgent, MultiOnRetrieverAgent
+
+__all__ = ["OpenInterpreterAgent", "MultiOnAgent", "MultiOnRetrieverAgent"]
